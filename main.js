bbb6e4fc5da7bcc5f9f5e4b7529db8e7d9310150
Bruna <108538735+Bruna096@users.noreply.github.com>
2023-09-20 12:25:42 -0300

    A

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ document.querySelector("#Som_Tecla_Pom").play();
 
 document.querySelector(".tecla_pom").onclick = tocaSomPom;
 
-//tecla Clap
+//tecla clap
 function tocaSomClap () {
     document.querySelector("#Som_Tecla_Clap").play();
     }
     
     document.querySelector(".tecla_Clap").onclick = tocaSomClap;
     
+//tecla Tim
+function tocaSomTim () {
+    document.querySelector("#Som_Tecla_Tim").play();
+    }
+    
+    document.querySelector(".tecla_Tim").onclick = tocaSomTim;
